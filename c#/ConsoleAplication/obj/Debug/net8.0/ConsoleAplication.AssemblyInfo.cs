b65f90b33cb8590a7e239579cb35117f35d28f44
andretokumoto <andretokumoto@gmail.com>
2024-10-18 11:35:48 -0300

    console c#

--- c#/ConsoleAplication/obj/Debug/net8.0/ConsoleAplication.AssemblyInfo.cs
+++ c#/ConsoleAplication/obj/Debug/net8.0/ConsoleAplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9210116e18b382b3c8de6188778f878044f5cdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+406346bea27a254acd8266e438ad694709dedff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
